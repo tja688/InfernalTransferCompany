eb4ea6095e1525d4c4197a32371c75f9eeae8325
Heliosly <woshisb06@gmail.com>
2025-09-23 23:50:54 +0800

    写了2脚本

---
+++ ITC/Assets/Scripts/Hedium/HoverAnimation.cs
@@ -0,0 +1,64 @@
+using PrimeTween;
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverAnimation : MonoBehaviour
+{
+    [SpineAnimation] public string hoverAnimation;
+    [SpineAnimation] public string idleAnimation;
+    private SkeletonAnimation skeletonAnimation;
+    public Transform targetImage;
+    
+    private bool isHovering = false;
+    private Tween scaleTween;
+    private Tween rotationTween;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+   
+    }
+
+    // Update is called once per frame
+    void OnMouseEnter()
+    {
+        if (isHovering) return; // ∑¿÷π÷ÿ∏¥¥•∑¢
+        
+        isHovering = true;
+        
+       
+        scaleTween.Stop();
+        rotationTween.Stop();
+
+        //skeletonAnimation.state.SetAnimation(0, hoverAnimation, true);
+
+        Vector3 aim = (Vector3.one * 1.1f);
+        aim.z = 1;
+        scaleTween = Tween.Scale(targetImage, aim, 0.5f, Ease.OutQuad);
+    
+        rotationTween = Tween.LocalRotation(targetImage, new Vector3(0f, 0f, 10f), 0.5f, Ease.OutQuad);
+    }
+
+    void OnMouseExit()
+    {
+        if (!isHovering) return; // ∑¿÷π÷ÿ∏¥¥•∑¢
+        
+        isHovering = false;
+        
+    
+        scaleTween.Stop();
+        rotationTween.Stop();
+        
+        // skeletonAnimation.state.SetAnimation(0, idleAnimation, true);
+        
+     
+        scaleTween = Tween.Scale(targetImage, Vector3.one, 0.5f, Ease.OutQuad);
+        rotationTween = Tween.LocalRotation(targetImage, Vector3.zero, 0.5f, Ease.OutQuad);
+       
+    }
+}
