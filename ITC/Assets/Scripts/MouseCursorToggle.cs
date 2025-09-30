c4d6c18c54be002ccc2ae4f4068c8e3a144ea021
Tang Jin <jinjintangtang2@gmail.com>
2025-09-30 11:03:37 +0800

    合并

---
+++ ITC/Assets/Scripts/MouseCursorToggle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Toggles the mouse cursor visibility when the N key is pressed.
+/// </summary>
+public class MouseCursorToggle : MonoBehaviour
+{
+    private bool isCursorVisible = true;
+
+    private void Start()
+    {
+        ApplyCursorState();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            isCursorVisible = !isCursorVisible;
+            ApplyCursorState();
+        }
+    }
+
+    private void ApplyCursorState()
+    {
+        Cursor.visible = isCursorVisible;
+        Cursor.lockState = isCursorVisible ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+}
