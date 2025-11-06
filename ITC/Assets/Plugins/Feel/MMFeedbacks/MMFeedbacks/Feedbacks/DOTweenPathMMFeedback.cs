2ffb44bcb89791632e3a06191af02eaba25e06b6
Tang Jin <156814864+tja688@users.noreply.github.com>
2025-11-06 16:25:01 +0800

    修复dotween问题

--- ITC/Assets/Plugins/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/DOTweenPathMMFeedback.cs
+++ ITC/Assets/Plugins/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/DOTweenPathMMFeedback.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG;
 using DG.Tweening;
+using UnityEngine.Scripting.APIUpdating;
 
 namespace MoreMountains.Feedbacks
 {
@@ -15,19 +16,32 @@ namespace MoreMountains.Feedbacks
     [FeedbackHelp("This feedback will let you pilot a DOTweenPath")]
     [FeedbackPath("DOTween/DOTween Path")]
     [Serializable]
+    [MovedFrom(false, null, "Assembly-CSharp")]
     public class DOTweenPathMMFeedback : MMF_Feedback
     {
         public enum Modes { DOPlay, DOPlayBackwards, DOPlayForward, DOPause, DOTogglePause, DORewind, DORestart, DOComplete, DOKill }
 
         [Header("DOTWeen Path")]
 
+        public static bool FeedbackTypeAuthorized = true;
+
+        [MMFInspectorGroup("DOTween Path", true, 54, true)]
+        [Tooltip("The DOTweenPath to control when this feedback plays")]
         public DOTweenPath TargetDOTweenPath;
 
+        [Tooltip("The action to perform on the DOTweenPath when this feedback plays")]
         public Modes Mode = Modes.DOPlay;
+
+#if UNITY_EDITOR
+        public override Color FeedbackColor => MMFeedbacksInspectorColors.AnimationColor;
+        public override bool EvaluateRequiresSetup() => TargetDOTweenPath == null;
+        public override string RequiredTargetText => TargetDOTweenPath != null ? TargetDOTweenPath.name : string.Empty;
+        public override string RequiresSetupText => "This feedback requires a DOTweenPath reference. You can set one below.";
+#endif
 
         protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f)
         {
-            if (Active == false || TargetDOTweenPath == null)
+            if (!Active || !FeedbackTypeAuthorized || (TargetDOTweenPath == null))
             {
                 return;
             }
