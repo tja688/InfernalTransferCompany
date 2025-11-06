27c8b84a42030ecd480a38dc0e8f58278cc988ea
Tang Jin <156814864+tja688@users.noreply.github.com>
2025-11-06 15:53:40 +0800

    导入bgm

--- ITC/Assets/Arts/Other_Resource/Materials/UIFrostedGlassController.cs
+++ ITC/Assets/Arts/Other_Resource/Materials/UIFrostedGlassController.cs
@@ -180,3 +180,4 @@ public class UIFrostedGlassController : MonoBehaviour
 
 
 
+
--- ITC/Assets/Plugins/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/DOTweenAnimationMMFeedback.cs
+++ ITC/Assets/Plugins/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/DOTweenAnimationMMFeedback.cs
@@ -6,54 +6,62 @@ using UnityEngine;
 using DG;
 using DG.Tweening;
 
-/// <summary>
-/// This feedback will let you pilot a DoTweenAnimation
-/// </summary>
-[AddComponentMenu("")]
-[FeedbackHelp("This feedback will let you pilot a DOTweenAnimation")]
-[FeedbackPath("DOTween/DOTween Animation")]
-public class DOTweenAnimationMMFeedback : MMFeedback
+namespace MoreMountains.Feedbacks
 {
-    public enum Modes { DOPlay, DOPlayBackwards, DOPlayForward, DOPause, DOTogglePause, DORewind, DORestart, DOComplete, DOKill }
-    
-    [Header("DOTWeen Animation")]
-    
-    public DOTweenAnimation TargetDOTweenAnimation;
-
-    public Modes Mode = Modes.DOPlay;
-    
-    protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f)
+    /// <summary>
+    /// This feedback will let you pilot a DOTweenAnimation
+    /// </summary>
+    [AddComponentMenu("")]
+    [FeedbackHelp("This feedback will let you pilot a DOTweenAnimation")]
+    [FeedbackPath("DOTween/DOTween Animation")]
+    [Serializable]
+    public class DOTweenAnimationMMFeedback : MMF_Feedback
     {
-        switch (Mode)
-        {
-            case Modes.DOPlay:
-                TargetDOTweenAnimation.DOPlay();
-                break;
-            case Modes.DOPlayBackwards:
-                TargetDOTweenAnimation.DOPlayBackwards();
-                break;
-            case Modes.DOPlayForward:
-                TargetDOTweenAnimation.DOPlayForward();
-                break;
-            case Modes.DOPause:
-                TargetDOTweenAnimation.DOPause();
-                break;
-            case Modes.DOTogglePause:
-                TargetDOTweenAnimation.DOTogglePause();
-                break;
-            case Modes.DORewind:
-                TargetDOTweenAnimation.DORewind();
-                break;
-            case Modes.DORestart:
-                TargetDOTweenAnimation.DORestart();
-                break;
-            case Modes.DOComplete:
-                TargetDOTweenAnimation.DOComplete();
-                break;
-            case Modes.DOKill:
-                TargetDOTweenAnimation.DOKill();
-                break;
-        }
+        public enum Modes { DOPlay, DOPlayBackwards, DOPlayForward, DOPause, DOTogglePause, DORewind, DORestart, DOComplete, DOKill }
+        
+        [Header("DOTWeen Animation")]
+        
+        public DOTweenAnimation TargetDOTweenAnimation;
 
+        public Modes Mode = Modes.DOPlay;
+        
+        protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f)
+        {
+            if (Active == false || TargetDOTweenAnimation == null)
+            {
+                return;
+            }
+            
+            switch (Mode)
+            {
+                case Modes.DOPlay:
+                    TargetDOTweenAnimation.DOPlay();
+                    break;
+                case Modes.DOPlayBackwards:
+                    TargetDOTweenAnimation.DOPlayBackwards();
+                    break;
+                case Modes.DOPlayForward:
+                    TargetDOTweenAnimation.DOPlayForward();
+                    break;
+                case Modes.DOPause:
+                    TargetDOTweenAnimation.DOPause();
+                    break;
+                case Modes.DOTogglePause:
+                    TargetDOTweenAnimation.DOTogglePause();
+                    break;
+                case Modes.DORewind:
+                    TargetDOTweenAnimation.DORewind();
+                    break;
+                case Modes.DORestart:
+                    TargetDOTweenAnimation.DORestart();
+                    break;
+                case Modes.DOComplete:
+                    TargetDOTweenAnimation.DOComplete();
+                    break;
+                case Modes.DOKill:
+                    TargetDOTweenAnimation.DOKill();
+                    break;
+            }
+        }
     }
 }
