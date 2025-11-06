7efc94753ba5c4b27e6b3975c50bb3536ea9fbc3
Tang Jin <156814864+tja688@users.noreply.github.com>
2025-11-06 16:25:01 +0800

    修复dotween问题

--- ITC/Assets/Plugins/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/DOTweenAnimationMMFeedback.cs
+++ ITC/Assets/Plugins/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/DOTweenAnimationMMFeedback.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG;
 using DG.Tweening;
+using UnityEngine.Scripting.APIUpdating;
 
 namespace MoreMountains.Feedbacks
 {
@@ -15,19 +16,32 @@ namespace MoreMountains.Feedbacks
     [FeedbackHelp("This feedback will let you pilot a DOTweenAnimation")]
     [FeedbackPath("DOTween/DOTween Animation")]
     [Serializable]
+    [MovedFrom(false, null, "Assembly-CSharp")]
     public class DOTweenAnimationMMFeedback : MMF_Feedback
     {
         public enum Modes { DOPlay, DOPlayBackwards, DOPlayForward, DOPause, DOTogglePause, DORewind, DORestart, DOComplete, DOKill }
         
         [Header("DOTWeen Animation")]
         
+        public static bool FeedbackTypeAuthorized = true;
+
+        [MMFInspectorGroup("DOTween Animation", true, 54, true)]
+        [Tooltip("The DOTweenAnimation to control when playing this feedback")] 
         public DOTweenAnimation TargetDOTweenAnimation;
 
+        [Tooltip("The action to perform on the DOTweenAnimation when this feedback plays")] 
         public Modes Mode = Modes.DOPlay;
+
+#if UNITY_EDITOR
+        public override Color FeedbackColor => MMFeedbacksInspectorColors.AnimationColor;
+        public override bool EvaluateRequiresSetup() => TargetDOTweenAnimation == null;
+        public override string RequiredTargetText => TargetDOTweenAnimation != null ? TargetDOTweenAnimation.name : string.Empty;
+        public override string RequiresSetupText => "This feedback requires a DOTweenAnimation reference. You can set one below.";
+#endif
         
         protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f)
         {
-            if (Active == false || TargetDOTweenAnimation == null)
+            if (!Active || !FeedbackTypeAuthorized || (TargetDOTweenAnimation == null))
             {
                 return;
             }
