db1ccb5d1e7b8a2d24fa5404995eb970346569c5
Tang Jin <156814864+tja688@users.noreply.github.com>
2025-11-19 14:58:47 +0800

    构建完整的游戏流状态机

---
+++ ITC/Assets/Scripts/唐今脚本/Core/Game Flow/GameEnums.cs
@@ -0,0 +1,11 @@
+namespace ITC.Core.GameFlow
+{
+    public enum GameState
+    {
+        Prologue,   // 序章
+        PreWork,    // 上班前
+        Work,       // 上班
+        PostWork,   // 下班 (自由探索)
+        Ending      // 结局
+    }
+}
